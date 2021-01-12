eeb326cbd31d29bb3ea971ffd6125b21557147fe
Irungaray <lionelirungaray@gmail.com>
2021-01-12 07:14:48 -0300

    Configured Layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import { Layout, SEO } from "../components"
+import { SEO } from "../components"
 import { Jumbo } from "../components"
 
 const IndexPage = () => (
-  <Layout>
+  <>
     <Jumbo />
     <SEO title="Home" />
     <h1>Hi people</h1>
@@ -13,7 +13,7 @@ const IndexPage = () => (
     <p>Now go build something great.</p>
     <Link to="/thanks/">Go to Thanks.</Link>
     <Link to="/sorry/">Go to Sorry .</Link>
-  </Layout>
+  </>
 )
 
 export default IndexPage
