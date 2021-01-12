baeab11eac156946119642944c46555d6babb0d0
Irungaray <lionelirungaray@gmail.com>
2021-01-12 05:55:36 -0300

    Thanks Component

---
+++ src/pages/thanks.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import { SEO } from '../components'
+import { Button, Purchase } from '../styles/components'
+
+export default function thanks() {
+  return (
+    <div>
+      <SEO title="Great purchase!" />
+
+      <Purchase>
+        <h2>Succesful Purchase!</h2>
+        <p>Thanks for buying.</p>
+        <p>Hope to see you again!</p>
+        <span rol='img' aria-label='emoji'>🙌</span>
+
+        <Link to='/'>
+          <Button>
+            Volver al catálogo
+          </Button>
+        </Link>
+      </Purchase>
+    </div>
+  )
+}
