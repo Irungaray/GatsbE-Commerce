916ec0da38ddb21318f7205975d063f9061a772d
Irungaray <lionelirungaray@gmail.com>
2021-01-12 16:16:09 -0300

    Added Images plugin

--- src/components/Jumbo.js
+++ src/components/Jumbo.js
@@ -1,14 +1,18 @@
-import React from "react"
+import React from 'react';
 
-import { StyledJumbo } from "../styles/components"
+import { StyledJumbo } from '../styles/components';
+
+import { Image } from './';
 
 export default function Jumbo({ description }) {
-  return (
-    <StyledJumbo>
-      <div>
-        <h2>Jumbo Store</h2>
-        <small>{description}</small>
-      </div>
-    </StyledJumbo>
-  )
+	return (
+		<StyledJumbo>
+			<div>
+				<h2>Jumbo Store</h2>
+				<small>{description}</small>
+			</div>
+
+			<Image name='icon' />
+		</StyledJumbo>
+	);
 }
