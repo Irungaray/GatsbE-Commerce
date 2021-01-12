de55d4add61757cc0d650398ba4b987918bb4dab
Irungaray <lionelirungaray@gmail.com>
2021-01-12 05:43:17 -0300

    First React Approach

---
+++ src/components/Jumbo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import {StyledJumbo} from '../styles/components';
+
+export default function Jumbo() {
+  return (
+    <StyledJumbo>
+      <div>
+        <h2>Jumbo Store</h2>
+        <small>Jumbo Store Chiquito</small>
+      </div>
+    </StyledJumbo>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,11 @@ import React from "react"
 import { Link } from "gatsby"
 
 import { Layout, SEO } from "../components"
+import { Jumbo } from "../components"
 
 const IndexPage = () => (
   <Layout>
+    <Jumbo />
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
