e8283fa1b549bcda68da2126ff11aad4b89e172a
Irungaray <lionelirungaray@gmail.com>
2021-01-14 08:18:45 -0300

    Added Context

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,21 +1,17 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
+// Implement Gatsby's Browser APIs in this file.
+// See: https://www.gatsbyjs.org/docs/browser-apis/
 
 const React = require('react');
 const Layout = require('./src/components/layout').default;
 const { GlobalStyles } = require('./src/styles');
+const { CartProvider } = require('./src/context');
 
 exports.wrapRootElement = ({ element }) => (
-	<>
+	<CartProvider>
 		<GlobalStyles />
 
 		<Layout>
 			{element}
 		</Layout>
-	</>
+	</CartProvider>
 );
--- src/components/header.js
+++ src/components/header.js
@@ -1,38 +1,45 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 
+import { CartContext } from '../context';
+
 import { MenuItem, StyledHeader } from '../styles/components';
 
-const Header = ({ siteTitle }) => (
-	<StyledHeader>
-		<Link to="/">
-			<img src="https://i.postimg.cc/6q3pg48v/Logo.png" alt="Logo" />
-		</Link>
+const Header = ({ siteTitle }) => {
+	const { cart } = useContext(CartContext);
 
-		<nav>
-			<ul>
-				<MenuItem margin>
-					<Link to="/">Products</Link>
-				</MenuItem>
+	return (
+		<StyledHeader>
+			<Link to="/">
+				<img src="https://i.postimg.cc/6q3pg48v/Logo.png" alt="Logo" />
+			</Link>
 
-				<MenuItem margin>
-					<a href="https://github.com/Irungaray" target="blank">
-            About
-					</a>
-				</MenuItem>
+			<nav>
+				<ul>
+					<MenuItem margin>
+						<Link to="/">Products</Link>
+					</MenuItem>
 
-				<MenuItem>
-					<Link to="/cart">
-						<span>
-							<img src="https://i.postimg.cc/L6wpMxLt/cart.png" alt="Cart" />
-						</span>
-					</Link>
-				</MenuItem>
-			</ul>
-		</nav>
-	</StyledHeader>
-);
+					<MenuItem margin>
+						<a href="https://github.com/Irungaray" target="blank">
+							About
+						</a>
+					</MenuItem>
+
+					<MenuItem>
+						<Link to="/cart">
+							<span>
+								<img src="https://i.postimg.cc/L6wpMxLt/cart.png" alt="Cart" />
+								{cart.length}
+							</span>
+						</Link>
+					</MenuItem>
+				</ul>
+			</nav>
+		</StyledHeader>
+	);
+};
 
 Header.propTypes = {
 	siteTitle: PropTypes.string,
